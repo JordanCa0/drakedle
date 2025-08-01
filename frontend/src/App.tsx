0dd331b7957dc9da45ede5bee9c8d5304eb212a8
Jordan Cao <34290789+JordanCa0@users.noreply.github.com>
2025-08-01 11:39:41 -0400

    Initialization and installation

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import './app.css'
+
+function App() {
+  return (
+    
+    <div className="flex flex-col items-center justify-center min-h-screen bg-neutral-900 p-4">
+      <title>Drakedle</title>
+      {/*Header*/}
+      <header className ="flex items-center justify-between p-4">
+        <h1 className = "text-2xl font-bold text-blue-200 tracking-wide">Drakedle</h1>
+      </header>
+
+      <main></main>
+
+    </div>
+  );
+}
+
+export default App;
