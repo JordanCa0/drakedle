e699ca43a19c9ff9b73cc33c2efb7af7701b526f
Jordan Cao <34290789+JordanCa0@users.noreply.github.com>
2025-08-03 15:57:05 -0400

    Added win state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
 import './app.css'
 import {useState} from "react";
+import anitaMaxWynn from "./assets/anitaMaxWynn.jpg";
+import anitaMaxWynnMp3 from "./assets/anitaMaxWynn.mp3";
 
 function App() {
+
+  let song: string = "hello";
+
+  const [isWin, setIsWin] = useState(false);
 
   const [guesses, setGuesses] = useState<string[]>([]);
 
@@ -10,6 +16,10 @@ function App() {
   const handleChange = (e: React.FormEvent) => {
     e.preventDefault();
     if (!inputValue.trim()) return;
+
+    if (inputValue.trim().toLowerCase() === song.toLowerCase()){
+      setIsWin(true);
+    }
 
     setGuesses (prev => {
       const updated = [...prev, inputValue.trim()].slice(-5);
@@ -25,23 +35,26 @@ function App() {
   return (
     
     <div className="flex flex-col items-center justify-center min-h-screen bg-neutral-900 p-4">
+
       <title>Drakedle</title>
       {/*Header*/}
       <header className ="flex items-center justify-between p-4">
         <h1 className = "text-2xl font-bold text-blue-200 tracking-wide">Drakedle</h1>
       </header>
 
-    <div className = "flex  flex-col gap-3 w-full max-W-xl mx-auto">
-      {boxes.map((guess,idx)=>(
-        <div
-          key = {idx}
-          className = "flex items-center justify-center h-16 boarder-2 rounded-lg bg-white/60 text-lg font-semibold text-gray-700 shadow"
-        >
-          {guess}
-        </div>
-      ))}
-    </div>
+        {/* Guess Boxes */}
+      <div className = "flex  flex-col gap-3 w-full max-W-xl mx-auto">
+        {boxes.map((guess,idx)=>(
+          <div
+            key = {idx}
+            className = "flex items-center justify-center h-16 boarder-2 rounded-lg bg-white/60 text-lg font-semibold text-gray-700 shadow"
+          >
+            {guess}
+          </div>
+        ))}
+      </div>
 
+      {/* Inputs */}
       <form onSubmit={handleChange} className='flex w-full max-w-xl  mx-auto mb-6 mt-3'>
         <input
           value = {inputValue}
@@ -56,6 +69,33 @@ function App() {
           Guess
         </button>
       </form>
+        
+      {/* Win State */}
+      {
+        isWin && (
+          <div className='fixed inset-50 bg-sky-300 flex items-center justify-center z-50 rounded-lg'>
+            <div className='bg-white rounded-lg p-8 shadow-xl text-2xl text-center'>
+
+            <p className="mt-4 text-green-600 font-bold">🎉 Correct! 🎉</p>
+
+            <button 
+            className='mt-4 bg-blue-600 text-white px-6 py-2 rounded'
+            onClick={()=>setIsWin(false)}
+            >Close</button>
+
+            <img 
+            className='mt-4 rounded-lg'
+            src={anitaMaxWynn} alt="Zesty Drake">
+            </img>
+
+            <audio src = {anitaMaxWynnMp3}>
+            </audio>
+
+            </div>
+          </div>
+        )
+      }
+
     </div>
         
   );
